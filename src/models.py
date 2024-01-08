b65aca74a07f22131d0d9a8fcff5247c20a56c20
Álvaro M. Villar <144016805+amv87@users.noreply.github.com>
2024-01-08 00:02:17 +0000

    added relationship to favorites on users class

--- src/models.py
+++ src/models.py
@@ -8,6 +8,8 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    favorite_people = db.relationship('FavoritePeople', lazy=True)
+    favorite_planets = db.relationship('FavoritePlanets', lazy=True)
 
     def __repr__(self):
         return '<User %r>' % self.email
@@ -16,8 +18,8 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
         }
+
     
 class People(db.Model):
     __tablename__ = 'people'
@@ -66,29 +68,12 @@ class Planets(db.Model):
             "rotation_period": self.rotation_period,
             "diameter": self.diameter,
         }
-
-class FavoritePlanets(db.Model):
-    __tablename__ = 'favorite_planets'
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    user = db.relationship('User')
-    planet_id = db.Column(db.Integer, db.ForeignKey('planets.id'), nullable=False)
-    planet = db.relationship('Planets')
-
-    def __repr__(self):
-        return '<FavoritePlanets %r>' % self.planet_id
     
-    def serialize(self):
-        return {
-            'user': self.user_id,
-            'favorite_planet': self.planet.name
-        }
-
 class FavoritePeople(db.Model):
     __tablename__ = 'favorite_people'
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    user = db.relationship('User')
+    user = db.relationship('User', back_populates='favorite_people')
     character_id = db.Column(db.Integer, db.ForeignKey('people.id'), nullable=False)
     character = db.relationship('People')
 
@@ -97,6 +82,21 @@ class FavoritePeople(db.Model):
     
     def serialize(self):
         return {
-            'user': self.user.email,
-            'favorite_character': self.character.name
+            'character_name': self.character.name
+        }
+
+class FavoritePlanets(db.Model):
+    __tablename__ = 'favorite_planets'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship('User', back_populates='favorite_planets')
+    planet_id = db.Column(db.Integer, db.ForeignKey('planets.id'), nullable=False)
+    planet = db.relationship('Planets')
+
+    def __repr__(self):
+        return '<FavoritePlanets %r>' % self.planet_id
+    
+    def serialize(self):
+        return {
+            'planet_name': self.planet.name
         }
